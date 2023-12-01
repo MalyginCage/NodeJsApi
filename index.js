758f242ca49ec0560df04c9d04e96e4228fd5aea
MalyginCage <dimacage@gmail.com>
2023-12-02 01:28:48 +0500

    v0.8

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ const mongoose = require('mongoose')
 
 
 
-
 const config = require('./config/app')
 mongoose.connect(config.mongoUrl, {useNewUrlParser: true, useUnifiedTopology: true}).then((res)=>console.log('Connected to MongoDB')).catch((err)=>console.log(`DB connection error: ${err}`))
 
+const signIn = (req,res)=>{
+    const {login, password}=req.body
+    User.findOne({login}).exec().then((user)=>{
+        if(!user){
+            res.status(401).json({message:'Логина нет'})
+        }
+        else{User.findOne({password}).exec().then((password)=>{if(!password){res.status(401).json({message:'Пароль неверный'})}}).catch(err => {
+            console.log(err)
+            res.status(500).json({error:err})
+        })}
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({error:err})
+    })
+}
 
 
 
@@ -56,6 +70,33 @@ app.get('/users/:_id', (req, res) => User.findById({_id: req.params._id}, req.bo
 
 
 app.post('/users', (req,res)=> User.create(req.body).then(createdUser => res.json(createdUser)))
+app.post('/signin',signIn)
+app.post('/signin', (req,res) => {
+    User.find({login: req.body.login}).select("_id login password role").exec().then(user => {
+        if(user.length < 1){
+            return res.status(401).json({
+                message: "Auth failed"
+            })
+        }
+        if(req.body.password!=user[0].password){
+            return res.status(401).json({
+                    message: "Auth failed"
+                })
+        }
+        else{
+            return res.status(200).json({
+                            message: "Auth successful",
+                            _id: user[0]._id,
+                        })
+        }
+ 
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error:err
+        })
+    })
+})
 
 app.put('/users/:_id', (req,res)=> User.findOneAndUpdate({_id: req.params._id}, req.body).exec().then(user => res.json(user)))
 
